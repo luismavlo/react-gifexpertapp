1a61472a4195dbbfb5a5c256ea2b011e6e1f910d
luismavlo <lmavendano1597@gmail.com>
2022-01-04 11:40:53 -0500

    first commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({category}) => {
+
+    const {data:images, loading} = useFetchGifs( category )
+
+    
+
+    return (
+        <>
+            <h3>{ category }</h3>
+            {
+                loading ? (
+                    <div class="loader"></div>
+                ):(
+                    <div className='grid-content'>
+                        {
+                            images.map(img => (
+                                <GifGridItem
+                                    key={img.id} 
+                                    {...img}
+                                />
+                            ))
+                        }
+                    </div> 
+                )
+            }
+              
+        </>
+    )
+}
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+import { getGift } from "../helpers/getGifs";
+
+export const useFetchGifs = ( category ) => {
+    
+    const [state, setstate] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(() => {
+        getGift( category )
+            .then( imgs => {   
+                    setstate({
+                        data: imgs,
+                        loading: false
+                    });
+            })
+    }, [category])
+
+    return state
+}
